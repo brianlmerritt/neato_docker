db2b9b6512abd21a0b9f46a57b80ab0320c538ae
brianlmerritt <brianlmerritt@noreply.com>
2022-03-06 11:23:09 +0000

    remove extra typings

--- ros2_entrypoint.sh
+++ ros2_entrypoint.sh
@@ -2,4 +2,4 @@
 #source /opt/ros/foxy/setup.bash
 #source /root/dev_ws/install/setup.bash
 tail -f /dev/null
-#ros2 launch botvac_node botvac_base.launch.py dock
+#ros2 launch botvac_node botvac_base.launch.py 
